97ab978dd86f93a0bad998feea3274d68fee1457
Betelhem-Belete <betelhembelete0@gmail.com>
2023-10-19 21:31:52 -0400

    sec

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import reportWebVitals from './reportWebVitals';
+
+import { Provider } from 'react';
+import { reducers } from './redux/reducers';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux'; //for connecting everything with react and
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
